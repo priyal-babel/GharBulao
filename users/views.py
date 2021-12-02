4950a2cfe29129e9078618c239c65c0db300a00b
Priyal Babel <priyal.babel@somaiya.edu>
2021-12-03 01:31:02 +0530

    UI changes UI completed
    Co-authored-by: Devansh Shah <DEV270201@users.noreply.github.com>
    Co-authored-by: krishvadhani19 <krishvadhani19@users.noreply.github.com>

--- users/views.py
+++ users/views.py
@@ -6,31 +6,27 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Img, Post, Reviews
 from django.db.models import Avg
-from django.contrib.auth.decorators import user_passes_test
-
+from ast import literal_eval
+from datetime import date
 from users.models import Profile
 from .forms import ProfileUpdateForm, UserRegisterForm, UserUpdateForm,PostForm, searchForm
 
-def user_is_not_logged_in(user):
-   return not user.is_authenticated()
-
-@user_passes_test(user_is_not_logged_in, login_url='/')
 def register(request):
- if not request.user:
-   if(request.method == 'POST'):
-      u_form = UserRegisterForm(request.POST)
-      # p_form = ProfileUpdateForm(request.POST,request.FILES or None)
-      if u_form.is_valid():
-         u_form.save()
-         # p_form.save()
-         username = u_form.cleaned_data.get('username')
-         print("username : " , username)
-         messages.success(request,f"Username {username}, your account is created successfully. Login to use the available features..")
-         return redirect('login')
+   if request.user!="AnonymousUser":
+      if(request.method == 'POST'):
+         u_form = UserRegisterForm(request.POST)
+         # p_form = ProfileUpdateForm(request.POST,request.FILES or None)
+         if u_form.is_valid():
+            u_form.save()
+            # p_form.save()
+            username = u_form.cleaned_data.get('username')
+            print("username : " , username)
+            messages.success(request,f"Username {username}, your account is created successfully. Login to use the available features..")
+            return redirect('login')
+      else:
+         u_form = UserRegisterForm()
+      return render(request,'users/register.html',{'u_form' : u_form})
    else:
-      u_form = UserRegisterForm()
-   return render(request,'users/register.html',{'u_form' : u_form})
- else:
       return redirect('home')
 
 @login_required
@@ -54,24 +50,30 @@ def profileUpdate(request):
       return render(request, 'users/profile.html',context)
 
 def home(request):
-   return render(request,'users/home.html')
+   services = [{"img" : "https://slowviking.com/wp-content/uploads/2017/08/Levers-Couchsurfing.jpg","title" : "Book Your Dream House"},{"img" : "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/hotel-booking-apps-1533324348.jpg","title" : "Post Yur Own Property"},{"img" : "https://www.thebrokebackpacker.com/wp-content/uploads/2016/07/CS.jpg","title":"Make New Memories"}]
+   return render(request,'users/home.html',{'services':services})
 
 @login_required
 def posts(request):
    
    if request.method == 'POST':
       pform = PostForm(request.POST)
-      images = request.FILES.getlist('image')
+      p = Post.objects.filter(user=request.user)
+      if p:
+          messages.warning(request, f'You have already posted your property.')
+          return redirect('post')
+      else:   
+         images = request.FILES.getlist('image')
 
-      if pform.is_valid():
-         pst = pform.save(commit=False)
-         pst.user = request.user
-         pst.save()
-         for img in images:
-            Img.objects.create(post=pst, image=img)
-         
-         messages.success(request, f'Your post has been updated.')
-         return redirect('post')
+         if pform.is_valid():
+            pst = pform.save(commit=False)
+            pst.user = request.user
+            pst.save()
+            for img in images:
+               Img.objects.create(post=pst, image=img)
+            
+            messages.success(request, f'Your post has been updated.')
+            return redirect('post')
    else:
       form = PostForm()
       context = {
@@ -81,11 +83,24 @@ def posts(request):
 
 @login_required
 def showPost(request,pk):
-   post = Post.objects.get(id=pk) 
+   info = {}
+   info['post'] = Post.objects.get(id=pk) 
+   info['images'] = Img.objects.filter(post=info['post']).defer('post')
+   info['reviews'] = Reviews.objects.filter(post=info['post']).defer('post')
+   icons = {
+      'wifi' : 'fas fa-wifi',
+      'TV' : 'fas fa-tv',
+      'music system' : 'fas fa-music',
+      'AC' : 'fas fa-wind',
+      'swimming pool' : 'fas fa-swimming-pool',
+      'gym' : 'fas fa-dumbbell',
+      'indoor sports' : 'fas fa-volleyball-ball'
+   }
+
    if request.POST.get('action') == '':
       review = request.POST.get('review')
       rating = request.POST.get('rating')
-      reviews  = Reviews.objects.filter(user=request.user,post=post)
+      reviews  = Reviews.objects.filter(user=request.user,post=info['post'] )
       if reviews:
          return HttpResponseNotFound('error message')
       else:
@@ -93,10 +108,12 @@ def showPost(request,pk):
             review = review,
             rating = rating,
             user = request.user,
-            post = post,
+            post = info['post'],
             )
          return HttpResponse('<html></html>')
-   return render(request,'users/showPost.html',context={'post':post})
+   info['post'].amenities = literal_eval(info['post'].amenities)
+   # print( info['post'].amenities , "     " , type(info['post'].amenities))
+   return render(request,'users/showPost.html', context={'info':info,'icons':icons})
 
 @login_required
 def postList(request):
@@ -104,6 +121,10 @@ def postList(request):
    review_post = []
    for r in r_post:
       x = [Img.objects.select_related().filter(post=r)]
+      if r.avg_rating:
+         r.avg_rating = round(r.avg_rating,1)
+      else:
+         r.avg_rating = 0
       x.append(r.avg_rating)
       review_post.append(x)
    form = searchForm()
@@ -144,6 +165,14 @@ def contact(request):
    except:
       return HttpResponseNotFound('<html></html>')
    return render(request,'users/contact.html')
+
+@login_required
+def receipt(request, pk):
+   post = Post.objects.get(id=pk)
+   img = Img.objects.filter(post=post)
+   post.date = date.today()
+   post.img = img[0].image
+   return render(request,'users/receipt.html',context={'post':post})
 
 # def getdata(request):
 #    p = Post.objects.all()
