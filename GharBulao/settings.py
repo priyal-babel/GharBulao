2bee24ec4418b15f18a86030e508972fcb11f3c6
Priyal Babel <priyal.babel@somaiya.edu>
2021-12-04 14:22:36 +0530

    Minor changes

--- GharBulao/settings.py
+++ GharBulao/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%##sg)qcfounhy*cw2wkh7!o+&rny73ny*@^a1k2x*@sb-%+w&'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
